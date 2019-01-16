8324c89256eacac9264a49830931d7b9845e11a0
wangkai60 <wangkai60@maoyan.com>
2019-01-16 17:57:46 +0800

    test case

---
+++ src/SortingBasic/SortingBasic/SelectionSortTestCase/SelectionSort.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019 maoyan.com
+ * All rights reserved.
+ *
+ */
+package SortingBasic.SortingBasic.SelectionSortTestCase;
+
+/**
+ * 在这里编写类的功能描述
+ *
+ * @author wangkai
+ * @created 2019/1/16
+ */
+public class SelectionSort {
+
+
+    //声明私有构造方法,不允许外部调用
+    private SelectionSort(){}
+
+    public static void sort(Comparable[] arr){
+        int n=arr.length;
+        for (int i=0;i<n;i++){
+            int min=i;
+            for (int j=i+1;j<n;j++){
+                if (arr[j].compareTo(arr[min])<0){
+                    min=j;
+                }
+            }
+            swap(arr,i,min);
+        }
+    }
+    public static void swap(Object[] arr,int i, int j){
+        Object t=arr[i];
+        arr[i] =arr[j];
+        arr[j] =t;
+    }
+
+    public static void main(String[] args) {
+
+        // 测试排序算法辅助函数
+        int N = 20;
+        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 100000);
+        SelectionSort.sort( arr );
+        SortTestHelper.printArray(arr);
+        return;
+    }
+}
---
+++ src/SortingBasic/SortingBasic/SelectionSortTestCase/SortTestHelper.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019 maoyan.com
+ * All rights reserved.
+ *
+ */
+package SortingBasic.SortingBasic.SelectionSortTestCase;
+
+/**
+ * 在这里编写类的功能描述
+ *
+ * @author wangkai
+ * @created 2019/1/16
+ */
+public class SortTestHelper {
+
+    private SortTestHelper(){}
+
+    //生成Test数组
+    public static  Integer[] generateRandomArray(int n,int rangL,int rangR){
+        assert rangL<=rangR;
+        Integer[] arr=new Integer[n];
+
+        for (int i=0;i<n;i++){
+            arr[i]=new Integer((int)(Math.random()*(rangR-rangL+1)+rangL));
+        }
+        return arr;
+    }
+    // 打印数组
+    public static void printArray(Object[] arr){
+        for (int i = 0; i < arr.length; i++){
+            System.out.print( arr[i] );
+            System.out.print( ' ' );
+        }
+        System.out.println();
+
+        return;
+    }
+}
